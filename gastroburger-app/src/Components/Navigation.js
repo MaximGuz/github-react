65b74fdc6555e7e8de1038657b1e163e31a2171a
MaximGuz <guz.maks@list.ru>
2023-08-21 22:07:28 +0300

    add

--- gastroburger-app/src/Components/Navigation.js
+++ gastroburger-app/src/Components/Navigation.js
@@ -1,14 +1,19 @@
 import styles from "./Navigation.module.css";
 import shopping_cart from "../images/free-icon-shopping-cart.png"
 import Button from "../UI/Button";
+import { useContext } from "react";
+import BasketContext from "../Context/BasketContext";
 
 const Navigation = (props) => {
+
+    const ctx = useContext(BasketContext);
+
     return (
       <nav className={styles.nav}>
         <ul>
             <li>
               <div className={styles.basket}>
-                <Button> <img src={shopping_cart} className={styles["shopping-cart"]}/> Корзина</Button>
+                <Button><img alt='Корзина' src={shopping_cart} className={styles["shopping-cart"]}/> Корзина <div className={styles['count-item']}>{ctx.totalCount}</div></Button>
               </div>
             </li>
         </ul>
